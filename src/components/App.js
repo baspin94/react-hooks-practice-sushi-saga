1d044f93f9711f72c32b1174d2d11228ef36da6f
Bianca Aspin <bianca@mj94.net>
2023-07-03 16:57:07 -0700

    Add state for sushi list, wallet, and empty plates; add logic for fetching sushi from API upon page load.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
+import { useState, useEffect } from "react";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([])
+  const [money, setMoney] = useState(50)
+  const [plates, setPlates] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => setSushi(data))
+  }, [])
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushi} money={money} setMoney={setMoney} plates={plates} setPlates={setPlates}/>
+      <Table plates={plates} money={money}/>
     </div>
   );
 }
