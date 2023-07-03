3eb979536f6fa3d767d652efccfa5d180c1c926e
Bianca Aspin <bianca@mj94.net>
2023-07-03 16:59:30 -0700

    Add logic for rendering limited amount of sushi at once with pagination, iterate through displayed sushi to create child components.

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
+import { useState, useEffect } from "react";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, money, setMoney, plates, setPlates}) {
+  const [start, setStart] = useState(0)
+  const [displaySushi, setDisplaySushi] = useState([])
+  
+  useEffect(() => {
+    setDisplaySushi(sushis.slice(start, start+4))
+  }, [start, sushis])
+
+  const sushiCards = displaySushi.map(sushi => {
+    return <Sushi key={sushi.id} details={sushi} money={money} setMoney={setMoney} plates={plates} setPlates={setPlates}/>
+  })
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiCards}
+      <MoreButton start={start} setStart={setStart}/>
     </div>
   );
 }
