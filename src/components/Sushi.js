c5678282587a5dd55df3270f4065b0df0e4afd9b
Bianca Aspin <bianca@mj94.net>
2023-07-03 17:01:31 -0700

    Render appropriate sushi details, add state to track whether sushi has been eaten, build out event handler for when sushi is clicked/eaten.

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
-function Sushi(props) {
+function Sushi({details, money, setMoney, plates, setPlates}) {
+  const [isEaten, setIsEaten] = useState(false)
+
+  function handleEat() {
+
+    if (money -  details.price >= 0) {
+      setIsEaten(true)
+      setMoney(money - details.price)
+      setPlates([...plates, details.name])
+    }
+    
+  }
+
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
-        {/* Tell me if this sushi has been eaten! */}
-        {false ? null : (
+      <div className="plate" onClick={handleEat}>
+        {isEaten ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={details.img_url}
+            alt={details.name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {details.name} - ${details.price}
       </h4>
     </div>
   );
