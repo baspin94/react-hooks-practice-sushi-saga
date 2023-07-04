821d825ae58cc12360c611ed2dff00567e2ebc1f
Bianca Aspin <bianca@mj94.net>
2023-07-03 17:05:01 -0700

    Add event handling logic for setting offset for sushi pagination.

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function MoreButton(props) {
-  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+function MoreButton({start, setStart}) {
+  function handleMore() {
+    setStart(start + 4)
+  }
+
+  return <button onClick={handleMore}>More sushi!</button>;
 }
 
 export default MoreButton;
